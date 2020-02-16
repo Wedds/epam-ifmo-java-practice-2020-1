9b7e9babd9307f76cef11ff007b217c25c82f7e4
HacksDev <mocrousov.and@yandex.ru>
2020-02-16 23:39:59 +0300

    Issue #17 Add autoincrement values

--- src/main/resources/Insert_test_script.sql
+++ src/main/resources/Insert_test_script.sql
@@ -7,6 +7,8 @@ INSERT INTO groups (id, name, created_at) values (4, 'M4120','2019-11-02');
 INSERT INTO groups (id, name, created_at) values (5, 'C2148','2020-01-04');
 INSERT INTO groups (id, name, created_at) values (6, 'I1238','2020-01-02');
 
+ALTER SEQUENCE groups_id_seq RESTART WITH 7;
+
 INSERT INTO users (id, role_type, email, hash, salt, first_name, last_name, middle_name, birth_date, work_title, created_at) values (1, 'admin', 'admin@pochta.ru', '45ec4bb100f5da97a4b939afbc1b6af2', '40a8903753c7d0780fa70ef3febd5f11', 'Donald', 'Tramp', 'Genadievich', '2000-01-20', 'programmer', '2020-02-11');
 INSERT INTO users (id, role_type, email, hash, salt, first_name, last_name, birth_date, work_title, created_at) values (2, 'teacher', 'teacher@pochta.ru', 'd46ffd73c3b76220f7ce65bc4ff26e30', 'e1cab46cgsdfshsfh9904a8a3b6743c', 'Абрикосик', 'Спелый', '1998-02-20', 'ЛУЧШИЙ УЧИТЕЛЬ ЕЕЕ', '2020-02-07');
 INSERT INTO users (id, role_type, email, hash, salt, first_name, last_name, middle_name, birth_date, work_title, created_at, group_id) values (3, 'student', 'student2@pochta.ru', 'd3267ce7e3247320c6194e7a5a16e18d', '2h32gho4u89fdsaghf0d6sad6t565ga8dyg', 'Светлана', 'Клавиатуровна', 'Константиновна','2003-02-20', 'Студент', '2020-01-23', 3);
@@ -14,16 +16,22 @@ INSERT INTO users (id, role_type, email, hash, salt, first_name, last_name, birt
 INSERT INTO users (id, role_type, email, hash, salt, first_name, last_name, middle_name, birth_date, work_title, created_at, group_id) values (5, 'student', 'student2@pochta.ru', 'd3267ce7e3247320c6194e7a5a16e18d', '2h32gho4u89fdsaghf0d6sad6t565ga8dyg', 'Светлана', 'Клавиатуровна', 'Константиновна','2003-02-20', 'Студент', '2020-01-23', 1);
 INSERT INTO users (id, role_type, email, hash, salt, first_name, last_name, birth_date, work_title, created_at) values (6, 'teacher', 'teacher2@pochta.ru', '4c6c76265940889d0392a6e48e57b6eb', 'jhgjg43jg23k1g423h5g1iu645264', 'Персик', 'Красный', '1998-02-20', 'Такой себе учитель', '2020-02-07');
 
+ALTER SEQUENCE users_id_seq RESTART WITH 7;
+
 INSERT INTO subjects (id, name) values (1, 'Math');
 INSERT INTO subjects (id, name) values (2, 'Programming');
 INSERT INTO subjects (id, name) values (3, 'English language');
 INSERT INTO subjects (id, name) values (4, 'Physics');
+
+ALTER SEQUENCE subjects_id_seq RESTART WITH 5;
 
 INSERT INTO tests (id, title, subject_id, is_random, creator_id, description, max_points) values (1, 'Wonderfull math', 1, TRUE, 2, 'ну, допустим, что это тест', 10);
 INSERT INTO tests (id, title, subject_id, is_random, creator_id, description, max_points) values (2, 'Progaprogapro', 2, FALSE, 6, 'все мы тут шли на прогеров', 15);
 INSERT INTO tests (id, title, subject_id, is_random, creator_id, description, max_points) values (3, 'Physics for chainycs', 4, FALSE, 2, 'а попали вот сюда', 10);
 INSERT INTO tests (id, title, subject_id, is_random, creator_id, description, max_points) values (4, 'And math again', 1, TRUE, 6, 'и нет теперь нам спасенья', 10);
 INSERT INTO tests (id, title, subject_id, is_random, creator_id, description, max_points) values (5,'SLOJNAAA', 3, TRUE, 2, 'памагииииити', 5);
+
+ALTER SEQUENCE tests_id_seq RESTART WITH 6;
 
 INSERT INTO groups_tests (group_id, test_id, is_necessary, max_attempts, time_limit, deadline) values (1, 3, FALSE, 10, 30, '2030-01-08');
 INSERT INTO groups_tests (group_id, test_id, is_necessary, max_attempts, time_limit, deadline) values (2, 1, TRUE, 1, 30, '2020-02-15');
@@ -38,6 +46,7 @@ INSERT INTO attempts (id, test_id, user_id, score, passing_date) VALUES (4, 5, 4
 INSERT INTO attempts (id, test_id, user_id, score, passing_date) VALUES (5, 5, 5, 2, '2020-02-03');
 INSERT INTO attempts (id, test_id, user_id, score, passing_date) VALUES (6, 3, 3, 5, '2020-02-06');
 
+ALTER SEQUENCE attempts_id_seq RESTART WITH 7;
 
 INSERT INTO questions (id, question_type, test_id, question_text) values (1, 'radiobutton', 1, '2 + 2 is?');
 INSERT INTO questions (id, question_type, test_id, question_text) values (2, 'checkbox', 1, 'x^2 = 4, x is?');
@@ -48,6 +57,8 @@ INSERT INTO questions (id, question_type, test_id, question_text) values (6, 'ra
 INSERT INTO questions (id, question_type, test_id, question_text) values (7, 'radiobutton', 3, 'You anyway will not understand it, so just try to guess');
 INSERT INTO questions (id, question_type, test_id, question_text) values (8, 'checkbox', 3, '1/0');
 
+ALTER SEQUENCE questions_id_seq RESTART WITH 9;
+
 INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (1, '4', 1, True, 2);
 INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (2, '5', 1, True, 2);
 INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (3, '2', 1, FALSE, -2);
@@ -56,3 +67,6 @@ INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (5
 INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (6, 'мало чет', 3, FALSE, -5);
 INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (7, 'Apple', 4, True, 5);
 INSERT INTO answers (id, answer_text, question_id, is_correct, points) VALUES (8, 'Android', 4, FALSE, -5);
+
+ALTER SEQUENCE answers_id_seq RESTART WITH 9;
+
