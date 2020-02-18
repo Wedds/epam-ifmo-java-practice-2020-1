b81fe6ee458e7c4a967c9dbc89bd115f9721a456
“Andrey <erdandrey@yandex.ru>
2020-02-18 23:42:41 +0300

    Updated entitiy & dao for Subjects

--- src/main/java/com/ifmo/epampractice/entity/Subjects.java
+++ src/main/java/com/ifmo/epampractice/entity/Subjects.java
@@ -1,4 +1,68 @@
 package com.ifmo.epampractice.entity;
 
 public class Subjects {
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Subjects {
+    private int id;
+    private String name;
+    private List<Tests> testsList;
+
+    public Subjects() {
+        this.testsList = new ArrayList<Tests>();
+    }
+
+    public List<Tests> getTestsList() {
+        return testsList;
+    }
+
+    public void setTestsList(List<Tests> testsList) {
+        this.testsList = testsList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Subjects subjects = (Subjects) o;
+        return id == subjects.id &&
+                name.equals(subjects.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Subjects{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", testsList=" + testsList +
+                '}';
+    }
 }
