ccc93508e93b9cb5ce071b28de4a3e5d8319a5ad
Esclide <esclide@yandex.ru>
2020-02-20 23:41:46 +0300

    Fix Answer entity #26

--- src/main/java/com/ifmo/epampractice/entity/Answers.java
+++ src/main/java/com/ifmo/epampractice/entity/Answers.java
@@ -60,7 +60,11 @@ public class Answers {
 
     @Override
     public boolean equals(Object o) {
-
+        if (this == o) {
+            return true;}
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Answers answers = (Answers) o;
         return id == answers.id &&
                 questionId == answers.questionId &&
