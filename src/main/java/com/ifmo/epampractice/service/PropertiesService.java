8cfc35db96b8178165a0708c184b55678ac18117
HacksDev <57772708+HacksDev@users.noreply.github.com>
2020-02-20 11:32:03 +0300

    Update PropertiesService.java

--- src/main/java/com/ifmo/epampractice/service/PropertiesService.java
+++ src/main/java/com/ifmo/epampractice/service/PropertiesService.java
@@ -6,29 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-//public class PropertiesService {
-//    private static final String DEFAULT_PATH = "src/test/resources/properties/";
-//
-//    public static class PropertyFile {
-//        private static String fileName;
-//
-//        public PropertyFile(String fileName) {
-//            PropertyFile.fileName = fileName;
-//        }
-//
-//        public String getPropsByName(String name) {
-//            Properties properties = new Properties();
-//            try {
-//                properties.load(new FileReader(DEFAULT_PATH + fileName));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return properties.getProperty(name);
-//        }
-//    }
-//
-//}
-
 
 public class PropertiesService {
     private static final String DEFAULT_FOLDER = "properties/";
