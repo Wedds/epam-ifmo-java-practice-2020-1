79f2d5ce352f0008d0cd0001d1f19b1dd07d8bc8
HacksDev <mocrousov.and@yandex.ru>
2020-02-20 03:07:12 +0300

    Issue #30 Update Properties Servie & Remove file

--- src/main/java/com/ifmo/epampractice/service/PropertiesService.java
+++ src/main/java/com/ifmo/epampractice/service/PropertiesService.java
@@ -1,28 +1,53 @@
 package com.ifmo.epampractice.service;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
+//public class PropertiesService {
+//    private static final String DEFAULT_PATH = "src/test/resources/properties/";
+//
+//    public static class PropertyFile {
+//        private static String fileName;
+//
+//        public PropertyFile(String fileName) {
+//            PropertyFile.fileName = fileName;
+//        }
+//
+//        public String getPropsByName(String name) {
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(new FileReader(DEFAULT_PATH + fileName));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return properties.getProperty(name);
+//        }
+//    }
+//
+//}
+
+
 public class PropertiesService {
-    private static final String DEFAULT_PATH = "src/test/resources/properties/";
+    private static final String DEFAULT_FOLDER = "properties/";
+    private Properties props;
 
-    public static class PropertyFile {
-        private static String fileName;
-
-        public PropertyFile(String fileName) {
-            PropertyFile.fileName = fileName;
-        }
-
-        public String getPropsByName(String name) {
-            Properties properties = new Properties();
-            try {
-                properties.load(new FileReader(DEFAULT_PATH + fileName));
-            } catch (IOException e) {
-                e.printStackTrace();
+    public PropertiesService(String fileName) {
+        this.props = new Properties();
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream(DEFAULT_FOLDER + fileName)) {
+            if (stream == null) {
+                throw new FileNotFoundException();
             }
-            return properties.getProperty(name);
+            props.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public String getPropsByName(String name) {
+        return props.getProperty(name);
     }
 
 }
