f11075aba5dac4a2712c0a33172080ae2a63c929
Esclide <esclide@yandex.ru>
2020-02-21 14:46:48 +0300

    Rename and add Optional method to DAO interface #50

--- src/main/java/com/ifmo/epampractice/service/DAO.java
+++ src/main/java/com/ifmo/epampractice/service/DAO.java
@@ -1,10 +1,11 @@
 package com.ifmo.epampractice.service;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface IDAO<T> {
+public interface DAO<T> {
 
-    T addObject(T object);
+    Optional<T> addObject(T object);
 
     List<T> getAll();
 
