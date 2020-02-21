29c7aabf0c95e65c24eb021c7a45b20da78bb0a7
Arahaon <grey.yurov@yandex.ru>
2020-02-21 13:15:45 +0000

    Issue#27 Resolve rebase(develop) conflicts & Fix checkstyle warnings

--- src/main/java/com/ifmo/epampractice/dao/GroupsDAO.java
+++ src/main/java/com/ifmo/epampractice/dao/GroupsDAO.java
@@ -21,7 +21,7 @@ public class GroupsDAO extends DatabaseSource implements IDAO<Groups> {
     private static final String REMOVE_QUERY = "DELETE FROM groups WHERE id=?";
 
     @Override
-    public Groups addObject(Groups group) {
+    public Groups addObject(final Groups group) {
         try (Connection connection = getConnection(); PreparedStatement preparedStatement =
                 connection.prepareStatement(ADD_QUERY)) {
             preparedStatement.setString(1, group.getName());
