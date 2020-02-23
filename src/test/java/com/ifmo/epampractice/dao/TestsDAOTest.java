2502dec3d397fe0bf8c08d6e7f3431b84d90bde9
Esclide <esclide@yandex.ru>
2020-02-23 19:57:14 +0300

    Issue #46 - change test methods names & add constants

--- src/test/java/com/ifmo/epampractice/dao/TestsDAOTest.java
+++ src/test/java/com/ifmo/epampractice/dao/TestsDAOTest.java
@@ -7,65 +7,78 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.Optional;
+import java.sql.Date;
 
 
 public class TestsDAOTest {
     private static final TestsDAO TEST_DAO = new TestsDAO();
+    private static final String TEST_TITLE = "Test test";
+    private static final String TEST_DESCRIPTION = "First test test";
+    private static final int SUBJECT_ID = 2;
+    private static final boolean IS_RANDOM = Boolean.FALSE;
+    private static final Date CREATION_DATE = Date.valueOf("2020-02-02");
+    private static final int TEST_MAX_POINTS = 20;
+    private static final int CREATOR_ID = 2;
+    private static final int GROUP_ID = 1;
+    private static final boolean IS_NECESSARY = Boolean.TRUE;
+    private static final int MAX_ATTEMPTS = 2;
+    private static final Date DEADLINE = Date.valueOf("2020-03-03");
+    private static final int TIME_LIMIT = 30;
+    private static final String TEST_TITLE_UPDATE = "Update test";
+    private static final String TEST_DESCRIPTION_UPDATE = "OLOLOLOLO";
+    private static final int SUBJECT_ID_UPDATE = 3;
+    private static final boolean IS_RANDOM_UPDATE = Boolean.TRUE;
+    private static final Date CREATION_DATE_UPDATE = Date.valueOf("2022-02-02");
+    private static final int TEST_MAX_POINTS_UPDATE = 10;
+    private static final int CREATOR_ID_UPDATE = 3;
+    private static final boolean IS_NECESSARY_UPDATE = Boolean.FALSE;
+    private static final int MAX_ATTEMPTS_UPDATE = 2;
+    private static final Date DEADLINE_UPDATE = Date.valueOf("2021-04-01");
+    private static final int TIME_LIMIT_UPDATE = 30;
+
 
     public Tests createTestsObject() {
         Tests test = new Tests();
-        test.setTitle("Test test");
-        test.setDescription("First test test");
-        test.setSubjectId(2);
-        test.setIsRandom(Boolean.FALSE);
-        java.sql.Date date = java.sql.Date.valueOf("2020-02-02");
-        test.setCreatedAt(date);
-        test.setMaxPoints(20);
-        test.setCreatorId(2);
+        test.setTitle(TEST_TITLE);
+        test.setDescription(TEST_DESCRIPTION);
+        test.setSubjectId(SUBJECT_ID);
+        test.setIsRandom(IS_RANDOM);
+        test.setCreatedAt(CREATION_DATE);
+        test.setMaxPoints(TEST_MAX_POINTS);
+        test.setCreatorId(CREATOR_ID);
         return test;
     }
 
     public Tests createGroupsTestsObject() {
         Tests test = createTestsObject();
-        test.setGroupId(1);
-        test.setIsNecessary(Boolean.TRUE);
-        test.setMaxAttempts(2);
-        java.sql.Date deadline = java.sql.Date.valueOf("2020-03-03");
-        test.setDeadline(deadline);
-        test.setTimeLimit(30);
-        java.sql.Date date = java.sql.Date.valueOf("2020-02-02");
-        test.setCreatedAt(date);
-        test.setMaxPoints(20);
-        test.setCreatorId(2);
+        test.setGroupId(GROUP_ID);
+        test.setIsNecessary(IS_NECESSARY);
+        test.setMaxAttempts(MAX_ATTEMPTS);
+        test.setDeadline(DEADLINE);
+        test.setTimeLimit(TIME_LIMIT);
         return test;
     }
 
     public Tests createGroupsTestsObjectForUpdate() {
         Tests test = new Tests();
-        test.setTitle("Update test");
-        test.setDescription("OLOLOLOLO");
-        test.setSubjectId(3);
-        test.setIsRandom(Boolean.TRUE);
-        java.sql.Date birthday = java.sql.Date.valueOf("2022-02-02");
-        test.setCreatedAt(birthday);
-        test.setMaxPoints(10);
-        test.setCreatorId(3);
-        test.setGroupId(1);
-        test.setIsNecessary(Boolean.FALSE);
-        test.setMaxAttempts(2);
-        java.sql.Date deadline = java.sql.Date.valueOf("2021-04-01");
-        test.setDeadline(deadline);
-        test.setTimeLimit(30);
-        java.sql.Date date = java.sql.Date.valueOf("2020-02-01");
-        test.setCreatedAt(date);
-        test.setMaxPoints(20);
-        test.setCreatorId(2);
+        test.setTitle(TEST_TITLE_UPDATE);
+        test.setDescription(TEST_DESCRIPTION_UPDATE);
+        test.setSubjectId(SUBJECT_ID_UPDATE);
+        test.setIsRandom(IS_RANDOM_UPDATE);
+        test.setCreatedAt(CREATION_DATE_UPDATE);
+        test.setMaxPoints(TEST_MAX_POINTS_UPDATE);
+        test.setCreatorId(CREATOR_ID_UPDATE);
+        test.setGroupId(GROUP_ID);
+        test.setIsNecessary(IS_NECESSARY_UPDATE);
+        test.setMaxAttempts(MAX_ATTEMPTS_UPDATE);
+        test.setDeadline(DEADLINE_UPDATE);
+        test.setTimeLimit(TIME_LIMIT_UPDATE);
         return test;
     }
 
     @Test
-    public void addObject() {
-        Boolean controlSum;
+    public void testAddObject() {
+        boolean controlSum;
         Tests test = createTestsObject();
         test = TEST_DAO.addObject(test);
         controlSum = TEST_DAO.getById(test.getId()).isPresent();
@@ -73,12 +86,12 @@ public class TestsDAOTest {
     }
 
     @Test
-    public void addTestsWithGroupsTests() {
-        Boolean controlSum;
+    public void testAddTestsWithGroupsTests() {
+        boolean controlSum;
         Tests test = createGroupsTestsObject();
         test = TEST_DAO.addTestsWithGroupsTests(test);
         Optional<Tests> testOptional = TEST_DAO.getObjectByTestAndGroupId(test.getId(), test.getGroupId());
-        if (testOptional.isPresent()){
+        if (testOptional.isPresent()) {
             controlSum = Boolean.TRUE;
         } else {
             controlSum = Boolean.FALSE;
@@ -87,8 +100,8 @@ public class TestsDAOTest {
     }
 
     @Test
-    public void addGroupsTests() {
-        Boolean controlSum;
+    public void testAddGroupsTests() {
+        boolean controlSum;
         Tests test = createGroupsTestsObject();
         test = TEST_DAO.addObject(test);
         test = TEST_DAO.addGroupsTests(test);
@@ -97,8 +110,8 @@ public class TestsDAOTest {
     }
 
     @Test
-    public void updateByObject() {
-        Boolean controlSum;
+    public void testUpdateByObject() {
+        boolean controlSum;
         Tests testBeforeUpdate = createGroupsTestsObject();
         testBeforeUpdate = TEST_DAO.addTestsWithGroupsTests(testBeforeUpdate);
         int testId = testBeforeUpdate.getId();
@@ -119,8 +132,8 @@ public class TestsDAOTest {
     }
 
     @Test
-    public void removeById() {
-        Boolean controlSum;
+    public void testRemoveById() {
+        boolean controlSum;
         Tests test = createGroupsTestsObject();
         test = TEST_DAO.addTestsWithGroupsTests(test);
         int id = test.getId();
@@ -130,8 +143,8 @@ public class TestsDAOTest {
     }
 
     @Test
-    public void fillObjectByGroupId() {
-        Boolean controlSum;
+    public void testFillObjectByGroupId() {
+        boolean controlSum;
         Tests testForInsert = createGroupsTestsObjectForUpdate();
         Tests testForCheck = TEST_DAO.addTestsWithGroupsTests(testForInsert);
         testForCheck = TEST_DAO.fillObjectByGroupId(testForCheck, testForInsert.getGroupId());
@@ -151,8 +164,8 @@ public class TestsDAOTest {
     }
 
     @Test
-    public void getById() {
-        Boolean controlSum;
+    public void testGetById() {
+        boolean controlSum;
         Tests test = createTestsObject();
         test = TEST_DAO.addObject(test);
         Optional<Tests> testOptional = TEST_DAO.getById(test.getId());
@@ -170,7 +183,7 @@ public class TestsDAOTest {
 
 
     @Test
-    public void getAll() {
+    public void testGetAll() {
         Tests test = createTestsObject();
         test = TEST_DAO.addObject(test);
         List<Tests> testsList = TEST_DAO.getAll();
