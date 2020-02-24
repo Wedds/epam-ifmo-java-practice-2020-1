afd88a7f9243d370cb015b17fb691c7a89730c2c
Esclide <esclide@yandex.ru>
2020-02-24 15:00:02 +0300

    Add tests for new methods and fix problems

--- src/test/java/com/ifmo/epampractice/dao/AttemptsDAOTest.java
+++ src/test/java/com/ifmo/epampractice/dao/AttemptsDAOTest.java
@@ -5,7 +5,6 @@ import com.ifmo.epampractice.entity.Attempts;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
 import java.util.Optional;
 import java.sql.Date;
 
@@ -19,6 +18,84 @@ public class AttemptsDAOTest {
     private static final int USER_ID_UPDATE = 2;
     private static final int SCORE_UPDATE = 5;
     private static final Date PASSING_DATE_UPDATE = Date.valueOf("2020-02-01");
+
+    @Test
+    public void testAddObject() {
+        boolean controlSum;
+        Attempts attempt = createAttemptsObject();
+        attempt = ATTEMPTS_DAO.addObject(attempt);
+        controlSum = ATTEMPTS_DAO.getById(attempt.getId()).isPresent();
+        Assert.assertEquals(Boolean.TRUE, controlSum);
+        ATTEMPTS_DAO.removeById(attempt.getId());
+    }
+
+    @Test
+    public void testGetAttemptsListByTestId() {
+        int wasElements = ATTEMPTS_DAO.getAttemptsListByTestId(2).size();
+        Attempts attempt = createAttemptsObject();
+        attempt = ATTEMPTS_DAO.addObject(attempt);
+        Assert.assertEquals(wasElements+1, ATTEMPTS_DAO.getAttemptsListByTestId(2).size());
+        ATTEMPTS_DAO.removeById(attempt.getId());
+    }
+
+    @Test
+    public void testGetAttemptsListByTestAndUserId() {
+        int wasElements = ATTEMPTS_DAO.getAttemptsListByTestAndUserId(2, 3).size();
+        Attempts attempt = createAttemptsObject();
+        attempt = ATTEMPTS_DAO.addObject(attempt);
+        Assert.assertEquals(wasElements+1, ATTEMPTS_DAO.getAttemptsListByTestAndUserId(2, 3).size());
+        ATTEMPTS_DAO.removeById(attempt.getId());
+    }
+
+    @Test
+    public void testGetAll() {
+        int wasElements = ATTEMPTS_DAO.getAll().size();
+        Attempts attempt = createAttemptsObject();
+        attempt = ATTEMPTS_DAO.addObject(attempt);
+        Assert.assertEquals(wasElements+1, ATTEMPTS_DAO.getAll().size());
+        ATTEMPTS_DAO.removeById(attempt.getId());
+    }
+
+    @Test
+    public void testGetById() {
+        Attempts attempt = createAttemptsObject();
+        attempt = ATTEMPTS_DAO.addObject(attempt);
+        Optional<Attempts> attemptOptional = ATTEMPTS_DAO.getById(attempt.getId());
+        Attempts receivedAnswer = new Attempts();
+        if (attemptOptional.isPresent()) {
+            receivedAnswer = attemptOptional.get();
+        }
+        Assert.assertEquals(Boolean.TRUE, receivedAnswer.equals(attempt));
+        ATTEMPTS_DAO.removeById(attempt.getId());
+    }
+
+    @Test
+    public void testUpdateByObject() {
+        Attempts attemptBeforeUpdate = createAttemptsObject();
+        attemptBeforeUpdate = ATTEMPTS_DAO.addObject(attemptBeforeUpdate);
+        int id = attemptBeforeUpdate.getId();
+        Attempts attemptForUpdate = createAttemptsObjectForUpdate();
+        attemptForUpdate.setId(id);
+        ATTEMPTS_DAO.updateByObject(attemptForUpdate);
+        Optional<Attempts> attemptOptional = ATTEMPTS_DAO.getById(id);
+        Attempts controlAttempt = new Attempts();
+        if (attemptOptional.isPresent()) {
+            controlAttempt = attemptOptional.get();
+        }
+        Assert.assertEquals(Boolean.TRUE, controlAttempt.equals(attemptForUpdate));
+        ATTEMPTS_DAO.removeById(attemptForUpdate.getId());
+    }
+
+    @Test
+    public void testRemoveById() {
+        boolean controlSum;
+        Attempts attempt = createAttemptsObject();
+        attempt = ATTEMPTS_DAO.addObject(attempt);
+        int id = attempt.getId();
+        ATTEMPTS_DAO.removeById(id);
+        controlSum = ATTEMPTS_DAO.getById(id).isPresent();
+        Assert.assertEquals(Boolean.FALSE, controlSum);
+    }
 
     public Attempts createAttemptsObject() {
         Attempts attempt = new Attempts();
@@ -36,89 +113,5 @@ public class AttemptsDAOTest {
         attempt.setScore(SCORE_UPDATE);
         attempt.setPassingDate(PASSING_DATE_UPDATE);
         return attempt;
-    }
-
-    @Test
-    public void testAddObject() {
-        boolean controlSum;
-        Attempts attempt = createAttemptsObject();
-        attempt = ATTEMPTS_DAO.addObject(attempt);
-        controlSum = ATTEMPTS_DAO.getById(attempt.getId()).isPresent();
-        Assert.assertEquals(Boolean.TRUE, controlSum);
-    }
-
-    @Test
-    public void testGetAttemptsListByTestId() {
-        Attempts attempt = createAttemptsObject();
-        attempt = ATTEMPTS_DAO.addObject(attempt);
-        List<Attempts> attemptsList = ATTEMPTS_DAO.getAttemptsListByTestId(2);
-        Assert.assertFalse(attemptsList.isEmpty());
-    }
-
-    @Test
-    public void testGetAttemptsListByTestAndUserId() {
-        Attempts attempt = createAttemptsObject();
-        attempt = ATTEMPTS_DAO.addObject(attempt);
-        List<Attempts> attemptsList = ATTEMPTS_DAO.getAttemptsListByTestAndUserId(2, 3);
-        Assert.assertFalse(attemptsList.isEmpty());
-    }
-
-    @Test
-    public void testGetAll() {
-        Attempts attempt = createAttemptsObject();
-        attempt = ATTEMPTS_DAO.addObject(attempt);
-        List<Attempts> attemptsList = ATTEMPTS_DAO.getAll();
-        Assert.assertFalse(attemptsList.isEmpty());
-    }
-
-    @Test
-    public void testGetById() {
-        boolean controlSum;
-        Attempts attempt = createAttemptsObject();
-        attempt = ATTEMPTS_DAO.addObject(attempt);
-        Optional<Attempts> attemptOptional = ATTEMPTS_DAO.getById(attempt.getId());
-        Attempts receivedAnswer = new Attempts();
-        if (attemptOptional.isPresent()) {
-            receivedAnswer = attemptOptional.get();
-        }
-        if (receivedAnswer.equals(attempt)) {
-            controlSum = Boolean.TRUE;
-        } else {
-            controlSum = Boolean.FALSE;
-        }
-        Assert.assertEquals(Boolean.TRUE, controlSum);
-    }
-
-    @Test
-    public void testUpdateByObject() {
-        boolean controlSum;
-        Attempts attemptBeforeUpdate = createAttemptsObject();
-        attemptBeforeUpdate = ATTEMPTS_DAO.addObject(attemptBeforeUpdate);
-        int id = attemptBeforeUpdate.getId();
-        Attempts attemptForUpdate = createAttemptsObjectForUpdate();
-        attemptForUpdate.setId(id);
-        ATTEMPTS_DAO.updateByObject(attemptForUpdate);
-        Optional<Attempts> attemptOptional = ATTEMPTS_DAO.getById(id);
-        Attempts controlAttempt = new Attempts();
-        if (attemptOptional.isPresent()) {
-            controlAttempt = attemptOptional.get();
-        }
-        if (controlAttempt.equals(attemptForUpdate)) {
-            controlSum = Boolean.TRUE;
-        } else {
-            controlSum = Boolean.FALSE;
-        }
-        Assert.assertEquals(Boolean.TRUE, controlSum);
-    }
-
-    @Test
-    public void testRemoveById() {
-        boolean controlSum;
-        Attempts attempt = createAttemptsObject();
-        attempt = ATTEMPTS_DAO.addObject(attempt);
-        int id = attempt.getId();
-        ATTEMPTS_DAO.removeById(id);
-        controlSum = ATTEMPTS_DAO.getById(id).isPresent();
-        Assert.assertEquals(Boolean.FALSE, controlSum);
     }
 }
