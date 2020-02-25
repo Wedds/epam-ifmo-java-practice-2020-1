7ef8cc95acca84861c003fdd6bfe12ce8d60ab61
Arahaon <grey.yurov@yandex.ru>
2020-02-25 16:22:02 +0000

    Issue#83 Fixed merged test

--- src/test/java/com/ifmo/epampractice/dao/GroupsDAOTest.java
+++ src/test/java/com/ifmo/epampractice/dao/GroupsDAOTest.java
@@ -16,18 +16,18 @@ public class GroupsDAOTest {
         /* Initialization & Setting expected values*/
         final int year2017 = 2017;
         final int year2020 = 2020;
-        final int day19 = 19;
-        final int day17 = 17;
-        final int hour13 = 13;
-        final int minute10 = 10;
-        final int seconds50 = 50;
+        final int day3 = 3;
+        final int day10 = 10;
+        final int hour20 = 20;
+        final int minute38 = 38;
+        final int seconds40 = 40;
         Groups expectedGroup1 = new Groups();
         expectedGroup1.setId(1);
         expectedGroup1.setName("K3120");
-        expectedGroup1.setCreatedAt(LocalDateTime.of(year2020, Month.DECEMBER, day19, hour13, minute10, seconds50));
+        expectedGroup1.setCreatedAt(LocalDateTime.of(year2020, Month.JANUARY, day3, hour20, minute38, seconds40));
         Groups expectedGroup2 = new Groups();
         expectedGroup2.setName("AAA");
-        expectedGroup2.setCreatedAt(LocalDateTime.of(year2017, Month.NOVEMBER, day17, hour13, minute10, seconds50));
+        expectedGroup2.setCreatedAt(LocalDateTime.of(year2017, Month.NOVEMBER, day10, hour20, minute38, seconds40));
         Groups realGroup1 = null;
         Groups realGroup2 = null;
         GroupsDAO groupDao = new GroupsDAO();
@@ -41,14 +41,14 @@ public class GroupsDAOTest {
 
         /* Checking addObject */
         realGroup2 = groupDao.addObject(expectedGroup2);
-        expectedGroup2.setId(7);
+        expectedGroup2.setId(realGroup2.getId());
         Assert.assertEquals(expectedGroup2, realGroup2);
 
         /*Checking getAll */
         listGroups = groupDao.getAll();
         Assert.assertEquals(7, listGroups.size());
         Assert.assertEquals(expectedGroup1, listGroups.get(0));
-        Assert.assertEquals(expectedGroup2, listGroups.get(6));
+        Assert.assertEquals(expectedGroup2, listGroups.get(realGroup2.getId() - 1));
 
         /*Checking removeById */
         groupDao.removeById(7);
