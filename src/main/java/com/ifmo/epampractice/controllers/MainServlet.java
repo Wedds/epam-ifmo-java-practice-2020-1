4c4f65dc1f6bf637eebc90982152c58ddccc0a9c
HacksDev <mocrousov.and@yandex.ru>
2020-02-26 23:42:42 +0300

    Issue #95 Fix codestyle

--- src/main/java/com/ifmo/epampractice/controllers/MainServlet.java
+++ src/main/java/com/ifmo/epampractice/controllers/MainServlet.java
@@ -9,8 +9,12 @@ import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException {
         RequestDispatcher view = req.getRequestDispatcher("index.jsp");
-        view.forward(req, resp);
+        try {
+            view.forward(req, resp);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Cannot forward to file.", e);
+        }
     }
 }
