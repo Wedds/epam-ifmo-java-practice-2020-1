f6399528a4e2aa5e72d360b1de600cf6930299b4
esclide <A970b71c94d>
2020-02-26 14:47:59 +0300

    Issue #91 - Fix database scripts

--- src/main/resources/Database_script_test.sql
+++ src/main/resources/Database_script_test.sql
@@ -4,54 +4,52 @@ create type types AS ENUM ('checkbox', 'radiobutton');
 CREATE TABLE IF NOT EXISTS "subjects"
 (
     "id"   SERIAL PRIMARY KEY,
-    "name" varchar UNIQUE
+    "name" varchar UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "groups"
 (
     "id"         SERIAL PRIMARY KEY,
-    "name"       varchar UNIQUE,
-    "created_at" Date
+    "name"       varchar UNIQUE  NOT NULL,
+    "created_at" timestamp NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "users"
 (
     "id"          SERIAL PRIMARY KEY,
-    "role_type"   roles,
-    "email"       varchar,
-    "hash"        varchar,
-    "salt"        varchar,
-    "first_name"  varchar,
-    "last_name"   varchar,
+    "role_type"   roles NOT NULL ,
+    "email"       varchar NOT NULL ,
+    "hash"        varchar NOT NULL ,
+    "salt"        varchar NOT NULL ,
+    "first_name"  varchar NOT NULL ,
+    "last_name"   varchar NOT NULL ,
     "middle_name" varchar,
-    "birth_date"  date,
+    "birth_date"  date NOT NULL ,
     "work_title"  varchar,
-    "created_at"  Date,
+    "created_at"  timestamp NOT NULL ,
     "avatar"      varchar,
-    "group_id"    int REFERENCES "groups" ("id")
+    "group_id"    int REFERENCES "groups" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-
-
 
 CREATE TABLE IF NOT EXISTS "tests"
 (
     "id"          SERIAL PRIMARY KEY,
-    "title"       varchar,
+    "title"       varchar NOT NULL ,
     "description" varchar,
-    "subject_id"  int REFERENCES "subjects" ("id"),
-    "is_random"   boolean,
-    "created_at"  Date,
-    "max_points"  int,
-    "creator_id"  int
+    "subject_id"  int NOT NULL REFERENCES "subjects" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "is_random"   boolean NOT NULL ,
+    "created_at"  timestamp NOT NULL ,
+    "max_points"  int NOT NULL ,
+    "creator_id"  int NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "groups_tests"
 (
-    "test_id"      int REFERENCES "tests" ("id"),
-    "group_id"     int REFERENCES "groups" ("id"),
-    "is_necessary" boolean,
-    "max_attempts" int DEFAULT 3,
-    "deadline"     Date,
+    "test_id"      int NOT NULL REFERENCES "tests" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "group_id"     int NOT NULL REFERENCES "groups" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "is_necessary" boolean NOT NULL ,
+    "max_attempts" int,
+    "deadline"     timestamp,
     "time_limit"   int,
     PRIMARY KEY ("test_id", "group_id")
 );
@@ -59,28 +57,28 @@ CREATE TABLE IF NOT EXISTS "groups_tests"
 CREATE TABLE IF NOT EXISTS "questions"
 (
     "id"            SERIAL PRIMARY KEY,
-    "question_type" types,
+    "question_type" types NOT NULL ,
     "title"         varchar,
     "image"         varchar,
-    "question_text" varchar,
-    "test_id"       int REFERENCES "tests" ("id")
+    "question_text" varchar NOT NULL ,
+    "test_id"       int NOT NULL REFERENCES "tests" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "answers"
 (
     "id"          SERIAL PRIMARY KEY,
     "image"       varchar,
-    "answer_text" varchar,
-    "question_id" int REFERENCES "questions" ("id"),
-    "is_correct"  boolean,
-    "points"      int
+    "answer_text" varchar NOT NULL ,
+    "question_id" int NOT NULL REFERENCES "questions" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "is_correct"  boolean NOT NULL ,
+    "points"      int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "attempts"
 (
     "id"           SERIAL PRIMARY KEY,
-    "test_id"      int REFERENCES "tests" ("id"),
-    "user_id"      int REFERENCES "users" ("id"),
-    "score"        int,
-    "passing_date" Date
+    "test_id"      int NOT NULL REFERENCES "tests" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "user_id"      int NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "score"        int NOT NULL ,
+    "passing_date" timestamp NOT NULL
 );
