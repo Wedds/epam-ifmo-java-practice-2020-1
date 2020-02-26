0708b60e1fa149d1d3b4aa1b60d88614eddea615
esclide <A970b71c94d>
2020-02-26 14:25:09 +0300

    Issue #70 - Add tests services

--- src/main/java/com/ifmo/epampractice/service/TestsService.java
+++ src/main/java/com/ifmo/epampractice/service/TestsService.java
@@ -1,22 +1,169 @@
 package com.ifmo.epampractice.service;
 
+import com.ifmo.epampractice.dao.TestsDAO;
 import com.ifmo.epampractice.entity.Tests;
+
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Optional;
 
 public class TestsService {
-    private static TestsService instance;
+    private  static final TestsDAO TESTS_DAO = new TestsDAO();
+    private static final QuestionsService QUESTIONS_SERVICE = new QuestionsService();
+    private static final AttemptsService ATTEMPTS_SERVICE = new AttemptsService();
 
-    public static TestsService getInstance() {
-        if (instance != null) {
-            return instance;
-        } else {
-            instance = new TestsService();
-        }
-        return instance;
+    public Tests addObject(final Tests test) {
+        // ПРВЕРИТЬ НА ЮЗЕРА И САБДЖЕКТ
+        return TESTS_DAO.addObject(test);
+
     }
 
-    public List<Tests> getAllTestsForGroupsByGroupId(final int id) {
-        return new ArrayList<Tests>();
+    public Tests addTestForGroup(final Tests test) {
+        // ПРОВЕРИТЬ НА ГРУППУ
+        if (ifTestObjectExist(test.getId())) {
+            System.err.println("This test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        return TESTS_DAO.addTestForGroup(test);
+
+    }
+
+    public List<Tests> getAll() {
+        return TESTS_DAO.getAll();
+    }
+
+    public List<Tests> getAllTestsForGroupsByTestId(final int testId) {
+        List<Tests> testsList;
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        testsList = TESTS_DAO.getAllTestsForGroupsByTestId(testId);
+        return testsList;
+    }
+
+    public List<Tests> getAllTestsForGroupsByGroupId(final int groupId) {
+        // ПРОВЕРЯЕМ ПО ГРУППЕ
+        List<Tests> testsList;
+        testsList = TESTS_DAO.getAllTestsForGroupsByGroupId(groupId);
+        return testsList;
+    }
+
+    public Tests getById(final int testId) {
+        Tests test;
+        Optional<Tests> testsOptional = TESTS_DAO.getById(testId);
+        if (!testsOptional.isPresent()) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        test = testsOptional.get();
+        return test;
+    }
+
+    public Tests getObjectByTestAndGroupId(final int testId, final int groupId) {
+        Tests test;
+        Optional<Tests> testsOptional = TESTS_DAO.getObjectByTestAndGroupId(testId, groupId);
+        // Пороверяем по тесту и по группе
+        if (!testsOptional.isPresent()) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        test = testsOptional.get();
+        return test;
+    }
+
+    public void updateTestByObject(final Tests test) {
+        // ПРВЕРИТЬ НА ЮЗЕРА И САБДЖЕКТ;
+        TESTS_DAO.updateByObject(test);
+    }
+
+    public void updateTestForGroupByObject(final Tests test) {
+        // ПРВЕРИТЬ НА ЮЗЕРА И САБДЖЕКТ;
+        TESTS_DAO.updateByObject(test);
+    }
+
+    public void removeTestById(final int testId) {
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        TESTS_DAO.removeById(testId);
+    }
+
+    public void removeGroupsTestsByTestId(final int testId) {
+        //Проверяем на группу и тест
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        TESTS_DAO.removeGroupsTestsByTestId(testId);
+    }
+
+    public void removeGroupsTestsByGroupId(final int groupId) {
+        //Проверяем на группу
+        TESTS_DAO.removeGroupsTestsByTestId(groupId);
+    }
+
+    public void removeGroupsTestsByTestAndGroupId(final int testId, final int groupId) {
+        //Проверяем на группу и тест
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        TESTS_DAO.removeGroupsTestsByTestAndGroupId(testId, groupId);
+    }
+
+    public Tests getTestWithQuestionsByTestId(final int testId) {
+        Tests test;
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        test = getById(testId);
+        test.setQuestionsList(QUESTIONS_SERVICE.getQuestionsWithAnswersListByTestId(testId));
+        return test;
+    }
+
+    public Tests getTestForGroupWithQuestions(final int testId, final int groupId) {
+        Tests test;
+        // CHECK GROUP
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        test = getObjectByTestAndGroupId(testId, groupId);
+        test.setQuestionsList(QUESTIONS_SERVICE.getQuestionsWithAnswersListByTestId(testId));
+        return test;
+    }
+
+    public Tests getTestWithAttemptsByTestId(final int testId) {
+        Tests test;
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        test = getById(testId);
+        test.setAttemptsList(ATTEMPTS_SERVICE.getAttemptsListByTestId(testId));
+        return test;
+    }
+
+    public Tests getTestForGroupWithAttempts(final int testId, final int groupId) {
+        Tests test;
+        // CHECK GROUP
+        if (!ifTestObjectExist(testId)) {
+            System.err.println("Test doesn't exist");
+            throw new IllegalArgumentException("This object doesn't exist");
+        }
+        test = getObjectByTestAndGroupId(testId, groupId);
+        test.setAttemptsList(ATTEMPTS_SERVICE.getAttemptsListByTestId(testId));
+        return test;
+    }
+
+    //public Tests getTestForUserWithAttempts(final int testId, final int userId) {
+
+    public Boolean ifTestObjectExist(final int id) {
+        if (TESTS_DAO.getById(id).isPresent()) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 }
